33b9d96df580ee1d33456de999dc6c1fbf9afbae
Liam <37994@ma-web.nl>
2024-03-01 10:15:42 +0100

    les1opdracht4

---
+++ m3prog_project/public/01/variabelen.php
@@ -0,0 +1,19 @@
+<?php
+ $naam = 'Liam';
+ $woonplaats = 'Muiderwaard';
+ $Leeftijd = 17;
+ $temp = 8.5;
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+        echo "hoi mijn naam is $naam en ik woon op de $woonplaats, ik ben $Leeftijd jaar oud en de temperatuur buiten is $temp."
+    ?>
+</body>
+</html>
