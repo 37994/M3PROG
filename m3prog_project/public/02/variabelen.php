af45bcc11892b5f1c7b4be88e697f8818540dbf0
Liam <37994@ma-web.nl>
2024-03-06 12:47:17 +0100

    les2opdracht2

---
+++ m3prog_project/public/02/variabelen.php
@@ -0,0 +1,14 @@
+<?php
+    $auto2 = 'lexus';
+    $_naam = 'Liam';
+    //$auto's = 'lexus, volvo';
+    //$autos/onderdelen = 'wheels';
+    //$huis&tuin = true;
+    $naw = 'filler';
+    //$22 = 'euros';
+    $auto = 'volvo';
+    $kleurVanAuto = 'pars';
+    //$software-dev = false;
+
+    echo $kleurVanAuto;
+?>
