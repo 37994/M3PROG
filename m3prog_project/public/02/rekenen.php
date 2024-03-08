dede1ee97d0eeeb6953041f81f0d75f7bb27f053
Liam <37994@ma-web.nl>
2024-03-08 10:06:01 +0100

    les2opdracht5

--- m3prog_project/public/02/rekenen.php
+++ m3prog_project/public/02/rekenen.php
@@ -1,6 +1,23 @@
 <?php
-    $leeftijd = 17;
-    $dagen_per_jaar = 365;
-    $leeftijd_in_dagen = $leeftijd * $dagen_per_jaar;
-    echo "Ik ben " . $leeftijd . " jaar " . " en dus al " . $leeftijd_in_dagen . " dagen oud!"; 
-?>
+
+$getal1 = 11214.489;
+$afgerond = round($getal1);
+echo "Als je {$getal1} afrond dan krijg je: {$afgerond}<br/>";
+
+$getal = 1.87321;
+$afgerond1 = ceil($getal);
+echo "$afgerond1 <br/>";
+
+$getal2 = 1.12873;
+$afgerond2 = floor($getal2);
+echo "$afgerond2 <br/>";
+
+$randomGetal1 = random_int(1, 999);
+$randomGetal2 = random_int(1, 999);
+$randomGetal3 = random_int(1, 999);
+
+$opgeteld = $randomGetal1 + $randomGetal2;
+echo "$randomGetal1 plus $randomGetal2 is $opgeteld. <br/>";
+
+$delen = $opgeteld / $randomGetal3;
+echo "$opgeteld gedeeld door $randomGetal3 is $delen";
